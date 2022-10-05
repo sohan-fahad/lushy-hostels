eee591b0c2153af4476af5275d6c87029d445784
sohan-fahad <sohan.bitspeck@gmail.com>
2022-10-05 15:37:23 +0600

    updated

--- src/Pages/HomePage/components/Navbar.svelte
+++ src/Pages/HomePage/components/Navbar.svelte
@@ -11,7 +11,7 @@
 		/>
 		<h3 class="text-lg sm:text-xl font-bold text-white pl-3 sm:pl-4 border-l-2">Lushy Hostels</h3>
 	</div>
-	<div class="pr-5 lg:pr-[150px] sm:pr-[50px] bg-secondary w-[40%] sm:w-[30%] lg:w-[25%]">
+	<div class="px-5 lg:px-[150px] sm:px-[50px] bg-secondary">
 		<a
 			class="h-full w-full text-right text-white font-semibold flex items-center justify-center lg:justify-end hover:text-white"
 			href="https://ibe.sabeeapp.com/properties/Lushy-Hostels-booking/LushyHostels.com?p=bSpd67901e440dade49&checkin=28-09-2022&checkout=29-09-2022"
